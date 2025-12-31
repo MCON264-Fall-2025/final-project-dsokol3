234366260004d18a589fc5c49bd5f3fe0b611d2f
dsokol3 <dsokol3@student.touro.edu>
2025-12-31 17:45:04 -0500

    junit tests

--- src/main/java/edu/course/eventplanner/service/GuestListManager.java
+++ src/main/java/edu/course/eventplanner/service/GuestListManager.java
@@ -6,11 +6,13 @@ import java.util.*;
 public class GuestListManager {
     private final LinkedList<Guest> guests = new LinkedList<>();
     private final Map<String, Guest> guestByName = new HashMap<>();
+
     public void addGuest(Guest guest) { /* TODO */
         guests.add(guest);
         guestByName.put(guest.getName(), guest);
-     }
-    public boolean removeGuest(String guestName) { 
+    }
+
+    public boolean removeGuest(String guestName) {
         Guest guest = guestByName.remove(guestName);
         if (guest != null) {
             guests.remove(guest);
@@ -18,7 +20,16 @@ public class GuestListManager {
         }
         return false;
     }
-    public Guest findGuest(String guestName) { return guestByName.get(guestName); }
-    public int getGuestCount() { return guests.size(); }
-    public List<Guest> getAllGuests() { return guests; }
+
+    public Guest findGuest(String guestName) {
+        return guestByName.get(guestName);
+    }
+
+    public int getGuestCount() {
+        return guests.size();
+    }
+
+    public List<Guest> getAllGuests() {
+        return guests;
+    }
 }
