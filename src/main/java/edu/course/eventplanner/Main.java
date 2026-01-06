29e5c16052a601f8190b83561a1e33ab1845d800
arobinso9 <arobinso9@student.touro.edu>
2026-01-06 17:27:57 -0500

    1-6-26 @ 5:27

--- src/main/java/edu/course/eventplanner/Main.java
+++ src/main/java/edu/course/eventplanner/Main.java
@@ -51,7 +51,7 @@ public class Main {
                     String nameToRemove = input.nextLine();
                     System.out.println("Whats the group tag of the guest you want to remove?");
                     String tagToRemove = input.nextLine();
-                    String nameAndGuest= nameToRemove+" "+tagToRemove;
+                    String nameAndGuest= nameToRemove+"-"+tagToRemove;
                     boolean isGuestRemoved= guestListManager.removeGuest(nameAndGuest);
                     if (isGuestRemoved) {
                         System.out.println(nameToRemove+ " with tag: "+tagToRemove+ " was successfully removed");
